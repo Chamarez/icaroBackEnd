a479164680c4ba705738f8422708f73147c18fbb
Michel <michel.chamarez@gmail.com>
2021-08-13 03:04:21 +0200

    get all users for new msj

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,7 +6,7 @@ import { checkRole } from "../middlewares/role";
 const router = Router();
 
 //get all users
-router.get('/', [checkJwt, checkRole(['admin'])], UserController.getAll);
+router.get('/', [checkJwt], UserController.getAll);
 
 //get one user
 router.get('/:id', [checkJwt, checkRole(['admin'])], UserController.getById);
